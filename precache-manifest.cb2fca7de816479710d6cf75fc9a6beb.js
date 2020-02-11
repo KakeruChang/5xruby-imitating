db8db7cc36cd78e7932534b83570af73c6d50ba9
Jim <b97201019@gmail.com>
2020-02-11 19:17:27 +0900

    Updates

---
+++ precache-manifest.cb2fca7de816479710d6cf75fc9a6beb.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5652e85088a8a24f419b500f1217947b",
+    "url": "/5xruby-imitating/index.html"
+  },
+  {
+    "revision": "14cb8f2ca482553713fd",
+    "url": "/5xruby-imitating/static/css/2.765f46be.chunk.css"
+  },
+  {
+    "revision": "d65a9b6a9bfa179169c7",
+    "url": "/5xruby-imitating/static/css/main.0c6d90ef.chunk.css"
+  },
+  {
+    "revision": "14cb8f2ca482553713fd",
+    "url": "/5xruby-imitating/static/js/2.ca5816c9.chunk.js"
+  },
+  {
+    "revision": "e5af15d6e625de31c63e2413f3280ce0",
+    "url": "/5xruby-imitating/static/js/2.ca5816c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d65a9b6a9bfa179169c7",
+    "url": "/5xruby-imitating/static/js/main.0f2ff262.chunk.js"
+  },
+  {
+    "revision": "dad5e99e78d3acb7d95b",
+    "url": "/5xruby-imitating/static/js/runtime-main.c068766c.js"
+  },
+  {
+    "revision": "e50d06e9f76885349502eec943dc473b",
+    "url": "/5xruby-imitating/static/media/banner_5xruby-3d66d288.e50d06e9.jpg"
+  },
+  {
+    "revision": "92048da11529d40c1fe343029b42d00c",
+    "url": "/5xruby-imitating/static/media/banner_astro-a839be5c.92048da1.jpg"
+  },
+  {
+    "revision": "06fc20c2c1e9f166ae30c6327841cf5a",
+    "url": "/5xruby-imitating/static/media/banner_imcodingit-53a9a811.06fc20c2.jpg"
+  },
+  {
+    "revision": "4ba5869166d1ddc90e17e267cf36532f",
+    "url": "/5xruby-imitating/static/media/banner_mokumokukai-f9ec6b54.4ba58691.jpg"
+  }
+]);
