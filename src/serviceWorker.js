ad2a06b6552e4089eaef0887ac40efb9584ca200
Jim <b97201019@gmail.com>
2020-02-15 09:11:38 +0900

    test for using Travis-CI

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -36,32 +36,36 @@ function registerValidSW(swUrl, config) {
               // At this point, the updated precached content has been fetched,
               // but the previous service worker will still serve the older
               // content until all client tabs are closed.
-              console.log(
-                'New content is available and will be used when all ' +
-                  'tabs for this page are closed. See https://bit.ly/CRA-PWA.'
-              )
+              // console.log(
+              //   'New content is available and will be used when all ' +
+              //     'tabs for this page are closed. See https://bit.ly/CRA-PWA.'
+              // )
 
               // Execute callback
               if (config && config.onUpdate) {
                 config.onUpdate(registration)
               }
-            } else {
-              // At this point, everything has been precached.
-              // It's the perfect time to display a
-              // "Content is cached for offline use." message.
-              console.log('Content is cached for offline use.')
+              // } else {
+              //   // At this point, everything has been precached.
+              //   // It's the perfect time to display a
+              //   // "Content is cached for offline use." message.
+              //   console.log('Content is cached for offline use.')
 
-              // Execute callback
-              if (config && config.onSuccess) {
-                config.onSuccess(registration)
-              }
+              //   // Execute callback
+              //   if (config && config.onSuccess) {
+              //     config.onSuccess(registration)
+              //   }
+              // }
+            } else if (config && config.onSuccess) {
+              config.onSuccess(registration)
             }
           }
         }
       }
     })
-    .catch(error => {
-      console.error('Error during service worker registration:', error)
+    // .catch(error => {
+    .catch(() => {
+      // console.error('Error during service worker registration:', error)
     })
 }
 
@@ -89,9 +93,9 @@ function checkValidServiceWorker(swUrl, config) {
       }
     })
     .catch(() => {
-      console.log(
-        'No internet connection found. App is running in offline mode.'
-      )
+      // console.log(
+      //   'No internet connection found. App is running in offline mode.'
+      // )
     })
 }
 
@@ -116,10 +120,10 @@ export function register(config) {
         // Add some additional logging to localhost, pointing developers to the
         // service worker/PWA documentation.
         navigator.serviceWorker.ready.then(() => {
-          console.log(
-            'This web app is being served cache-first by a service ' +
-              'worker. To learn more, visit https://bit.ly/CRA-PWA'
-          )
+          // console.log(
+          //   'This web app is being served cache-first by a service ' +
+          //     'worker. To learn more, visit https://bit.ly/CRA-PWA'
+          // )
         })
       } else {
         // Is not localhost. Just register service worker
--- webpack.config.js
+++ webpack.config.js
@@ -7,8 +7,7 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'index_bundle.js',
-    publicPath: '/'
+    filename: 'index_bundle.js'
   },
   module: {
     rules: [
@@ -36,10 +35,10 @@ module.exports = {
     ]
   },
   devServer: {
+    contentBase: './dist',
     historyApiFallback: true,
     inline: true,
-    port: 3000,
-    publicPath: '/'
+    port: 3000
   },
   plugins: [
     new HtmlWebpackPlugin({
